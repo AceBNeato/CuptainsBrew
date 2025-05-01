8d2dbff4764917a541d6488b63d970f07ac87ec1
GIAN GARCIA <giangarcia977@gmail.com>
2025-05-01 10:21:00 +0800

    update

---
+++ controllers/admin-controller.php
@@ -0,0 +1,14 @@
+<?php
+// controllers/admin-controller.php
+
+function showAdminMenu() {
+    require '../views/admin/Admin-Menu.php';
+}
+
+function showAdminOrders() {
+    require '../views/admin/Admin-Orders.php';
+}
+
+function showAdminReports() {
+    require '../views/admin/Admin-Reports.php';
+}
